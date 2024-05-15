cfc3569d7f1afad6dcd93e49325236c7f7385093
klayhopgood <62199299+klayhopgood@users.noreply.github.com>
2024-05-15 14:03:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function loadClient() {
-  const apiKey = "YOUR_API_KEY"; // Replace with your actual API key
-  const spreadsheetId = "YOUR_SPREADSHEET_ID"; // Replace with your spreadsheet ID (optional)
+  const apiKey = "AIzaSyDuEn3Mw1oaAEKISiiUUNZmC1X3UvYPF5k"; // Replace with your actual API key
+  const spreadsheetId = "17uxdu7E4G8v3nquImZVFu4jvaR_YB35uiocnb2yYh1k"; // Replace with your spreadsheet ID (optional)
 
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/17uxdu7E4G8v3nquImZVFu4jvaR_YB35uiocnb2yYh1k; // Build the API endpoint URL
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}; // Build the API endpoint URL
 
   fetch(url, {
     headers: {
